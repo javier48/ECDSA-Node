7a788189c21d57c882988d92601a6874553c88cc
javier48 <javierjulianda48@gmail.com>
2023-08-24 02:21:04 +0700

    -

---
+++ server/index.js
@@ -0,0 +1,44 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const port = 3042;
+
+app.use(cors());
+app.use(express.json());
+
+const balances = {
+  "03b7dc81fe7d89cece0958b8c71f726ea8c49db60260a1133f72c3fa12c8321eb0": 100,
+  "03ac6ecad5487d95fd4dd2d465a8191be59882ab23fd91cbd1ff4599799cf34519": 50,
+  "033785e65d8bc7c464fabb04abdf67284d77e2bf712c8e75ccb29fb245016adf9a": 25,
+};
+
+app.get("/balance/:address", (req, res) => {
+  const { address } = req.params;
+  const balance = balances[address] || 0;
+  res.send({ balance });
+});
+
+app.post("/send", (req, res) => {
+  const { sender, recipient, amount } = req.body;
+
+  setInitialBalance(sender);
+  setInitialBalance(recipient);
+
+  if (balances[sender] < amount) {
+    res.status(400).send({ message: "Not enough funds!" });
+  } else {
+    balances[sender] -= amount;
+    balances[recipient] += amount;
+    res.send({ balance: balances[sender] });
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}!`);
+});
+
+function setInitialBalance(address) {
+  if (!balances[address]) {
+    balances[address] = 0;
+  }
+}
